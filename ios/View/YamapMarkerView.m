a6e11ec9d5d586c9977a1756f5b03e6a4eb04040
Alexander Chernyavskiy <git@ch3rn1k.me>
2022-05-18 09:20:25 +0300

    fix: minimized marker redraw time on iOS

--- ios/View/YamapMarkerView.m
+++ ios/View/YamapMarkerView.m
@@ -140,8 +140,7 @@
 }
 
 - (void)didUpdateReactSubviews {
-    // todo: Если вызывать сразу то frame имеет размеры 0. В идеале делать подписку на событие
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+    dispatch_async(dispatch_get_main_queue(), ^{
         [self setChildView];
     });
 }
