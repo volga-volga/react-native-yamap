fab776c9e22efb9fb23c63c66c5446ff8205ef81
Alexander Chernyavskiy <git@ch3rn1k.me>
2022-05-26 09:39:30 +0300

    fix: code style misc

--- ios/View/RNYMView.m
+++ ios/View/RNYMView.m
@@ -286,8 +286,7 @@
 -(void) setCenter:(YMKCameraPosition*) position withDuration:(float) duration withAnimation:(int) animation {
     if (duration > 0) {
         YMKAnimationType anim = animation == 0 ? YMKAnimationTypeSmooth : YMKAnimationTypeLinear;
-        [self.mapWindow.map moveWithCameraPosition:position animationType:[YMKAnimation animationWithType:anim duration: duration] cameraCallback: ^(BOOL completed) {
-         }];
+        [self.mapWindow.map moveWithCameraPosition:position animationType:[YMKAnimation animationWithType:anim duration: duration] cameraCallback: ^(BOOL completed) {}];
     } else {
         [self.mapWindow.map moveWithCameraPosition:position];
     }
