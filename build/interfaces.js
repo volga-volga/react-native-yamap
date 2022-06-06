491451a8b27df011a6132b205bc13e0f91a873e4
DazzlingFame <narsel1995@mail.ru>
2022-06-06 12:47:19 +0300

    build remove

--- build/interfaces.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Animation = void 0;
-var Animation;
-(function (Animation) {
-    Animation[Animation["SMOOTH"] = 0] = "SMOOTH";
-    Animation[Animation["LINEAR"] = 1] = "LINEAR";
-})(Animation = exports.Animation || (exports.Animation = {}));
-//# sourceMappingURL=interfaces.js.map
