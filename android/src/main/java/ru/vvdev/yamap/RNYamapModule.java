622181554c1be48522fb330a8431e1f3593fabbd
Колесников Вячеслав <narsel1995@mail.ru>
2022-06-29 18:37:41 +0300

    Фикс падения приложения при hot reload в дев режиме (#134)
    * fix
    
    * add build
    
    * Promise import fix
    
    * Revert "add build"
    
    This reverts commit 712c041624de40d8da3e12ab35678600e39c6c11.

--- android/src/main/java/ru/vvdev/yamap/RNYamapModule.java
+++ android/src/main/java/ru/vvdev/yamap/RNYamapModule.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
@@ -45,14 +46,31 @@ public class RNYamapModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void init(final String apiKey) {
+    public void init(final String apiKey, final Promise promise) {
         runOnUiThread(new Thread(new Runnable() {
             @Override
             public void run() {
-                MapKitFactory.setApiKey(apiKey);
-                MapKitFactory.initialize(reactContext);
-                TransportFactory.initialize(reactContext);
-                MapKitFactory.getInstance().onStart();
+                Throwable apiKeyException = null;
+                try {
+                    // In case when android application reloads during development
+                    // MapKitFactory is already initialized
+                    // And setting api key leads to crash
+                    try {
+                        MapKitFactory.setApiKey(apiKey);
+                    } catch (Throwable exception) {
+                        apiKeyException = exception;
+                    }
+                    MapKitFactory.initialize(reactContext);
+                    TransportFactory.initialize(reactContext);
+                    MapKitFactory.getInstance().onStart();
+                    promise.resolve(null);
+                } catch (Exception exception) {
+                    if(apiKeyException != null) {
+                        promise.reject(apiKeyException);
+                        return;
+                    }
+                    promise.reject(exception);
+                }
             }
         }));
     }
