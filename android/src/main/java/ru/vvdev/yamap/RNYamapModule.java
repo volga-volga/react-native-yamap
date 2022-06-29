111f515202945537ab46b8270a084d454252bdd6
DazzlingFame <narsel1995@mail.ru>
2022-06-29 14:24:01 +0300

    Promise import fix

--- android/src/main/java/ru/vvdev/yamap/RNYamapModule.java
+++ android/src/main/java/ru/vvdev/yamap/RNYamapModule.java
@@ -5,6 +5,7 @@ import androidx.annotation.NonNull;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
