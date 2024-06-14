f97d4aee202be48cc451a8853923066657b5748d
alen-s <75077148+alen-s@users.noreply.github.com>
2024-06-14 13:21:45 +0400

    fix: issue-258, `geoToAddress()` method's results are configurable by language. (#259)
    Co-authored-by: Alen Simonyan <a.simonyan@vardot.com>

--- src/geocoding/index.ts
+++ src/geocoding/index.ts
@@ -98,8 +98,8 @@ export class Geocoder {
     return undefined;
   }
 
-  static async geoToAddress(geo: Point): Promise<Address | undefined> {
-    const { response } = await Geocoder.geocode(geo);
+  static async geoToAddress(geo: Point, lang?: Lang, kind?: ObjectKind): Promise<Address | undefined> {
+    const { response } = await Geocoder.geocode(geo, kind, 1, 0, lang);
 
     if (
       response.GeoObjectCollection
